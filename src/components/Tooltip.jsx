0146b1cfc98a259cc6befbd1557c11f663afb7ac
JuanSR <jfrancisco1717@gmail.com>
2020-05-07 03:26:12 -0400

    Update tooltip

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -8,7 +8,7 @@ const ColoredTooltip = () => {
 
 const CustomChild = forwardRef((props, ref) => {
     return (
-        <div ref={ ref }>
+        <div ref={ref}>
             <div>First line</div>
             <div>Second line</div>
         </div>
@@ -18,32 +18,26 @@ const CustomChild = forwardRef((props, ref) => {
 function Tooltip() {
     return (
         <div>
-            <div style={{ paddingBottom: '20px' }}>
+            <div style={{ paddingBottom: '20px', paddingTop: '20px' }}>
                 <Tippy
-                    placement='right'
-                    arrow={ false }
-                    delay={ 1000 } 
+                    placement='left'
+                    arrow={false}
+                    delay={1000} 
                     content='Basic Tooltip'
                 >
                     <button>Hover</button>
                 </Tippy>
-            </div>
 
-            <div style={{ paddingBottom: '20px' }}>
                 <Tippy 
                     content={<span style={{ color: 'orange' }}>Colored</span>}
                 >
                     <button>Hover</button>
                 </Tippy>
-            </div>
 
-            <div style={{ paddingBottom: '20px' }}>
                 <Tippy content={<ColoredTooltip />}>
                     <button>Hover</button>
                 </Tippy>
-            </div>
 
-            <div style={{ paddingBottom: '20px' }}>
                 <Tippy placement='top-start' content={<ColoredTooltip />}>
                     <CustomChild />
                 </Tippy>
